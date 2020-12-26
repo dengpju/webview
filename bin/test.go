c87443f9f2a5bdc008a84e42169a27fb40f04097
dengpengju <dengpju@163.com>
2020-12-26 10:54:04 +0800

    更新

--- bin/test.go
+++ bin/test.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/zserge/lorca"
+	"log"
+	"net/url"
+	"sync/atomic"
+	"time"
 )
 
 func main() {
+	/**
 	ui, _ := lorca.New("https://hsbcapi.51jiaoyujia.com/admin", "", 1280, 1024)
 	defer ui.Close()
 
@@ -22,5 +27,50 @@ func main() {
 	fmt.Println(m)
 
 	// Wait for the browser window to be closed
+	<-ui.Done()
+
+	 */
+	ui, err := lorca.New("", "", 480, 320)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ui.Close()
+
+	// Data model: number of ticks
+	ticks := uint32(0)
+	// Channel to connect UI events with the background ticking goroutine
+	togglec := make(chan bool)
+	// Bind Go functions to JS
+	ui.Bind("toggle", func() { togglec <- true })
+	ui.Bind("reset", func() {
+		atomic.StoreUint32(&ticks, 0)
+		ui.Eval(`document.querySelector('.timer').innerText = '0'`)
+	})
+
+	// Load HTML after Go functions are bound to JS
+	ui.Load("data:text/html," + url.PathEscape(`
+	<html>
+		<body>
+			<!-- toggle() and reset() are Go functions wrapped into JS -->
+			<div class="timer" onclick="toggle()"></div>
+			<button onclick="reset()">Reset</button>
+			<iframe src="https://hsbcapi.51jiaoyujia.com/admin"></iframe>
+		</body>
+	</html>
+	`))
+
+	// Start ticker goroutine
+	go func() {
+		t := time.NewTicker(100 * time.Millisecond)
+		for {
+			select {
+			case <-t.C: // Every 100ms increate number of ticks and update UI
+				ui.Eval(fmt.Sprintf(`document.querySelector('.timer').innerText = 0.1*%d`,
+					atomic.AddUint32(&ticks, 1)))
+			case <-togglec: // If paused - wait for another toggle event to unpause
+				<-togglec
+			}
+		}
+	}()
 	<-ui.Done()
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/dengpju/higo-redis v0.0.0-20201223150342-cabded821eda // indirect
 	github.com/lxn/walk v0.0.0-20201209144500-98655d01b2f1
 	github.com/lxn/win v0.0.0-20201111105847-2a20daff6a55 // indirect
+	github.com/zserge/lorca v0.1.9
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
 	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
 )
